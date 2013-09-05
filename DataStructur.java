a2f63d397973807337c350b677b1a81a3da7f1b2
Daniel Damgaard <daniel@damgaard.in>
2013-09-05 16:31:08 +0200

    Data struktur

---
+++ DataStructur.java
@@ -0,0 +1,41 @@
+package example;
+
+import java.util.List;
+import java.util.Vector;
+import main.Master;
+import org.pi4.locutil.MACAddress;
+import org.pi4.locutil.trace.SignalStrengthSamples;
+import org.pi4.locutil.trace.TraceEntry;
+
+public class DataStructur {
+    public static void Run(){
+        Master m = Master.Inst();
+        
+        for(TraceEntry entry: m.getOnlineTrace()) {
+            System.out.println(
+                "[TraceEntry] "+
+                "GeoPos: "+entry.getGeoPosition()+", "+
+                "Id / MAC: "+entry.getId()+", "+
+                "Speed: "+entry.getSpeed()+", "+
+                "Time: "+entry.getTimestamp()+"."
+            );
+            
+            SignalStrengthSamples sss = entry.getSignalStrengthSamples();
+            
+            List<MACAddress> aps = sss.getSortedAccessPoints();
+            
+            for(MACAddress mac : aps){
+                System.out.println(
+                    "[AccessPoint] "+
+                    "Id / MAC: "+mac+", "+
+                    "Strengths: "+sss.getSignalStrengthValues(mac)+", "+
+                    "Noises: "+sss.getNoiseValues(mac)+", "+
+                    "Avg. strength: "+sss.getAverageSignalStrength(mac)+", "+
+                    "Avg. noise: "+sss.getAverageNoise(mac)+", "+
+                    "Deviation: "+sss.getStandardDeviationSignalStrength(mac)+", "+
+                    "Variance: "+sss.getVarianceSignalStrength(mac)
+                );
+            }
+        }
+    }
+}
